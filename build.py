599401d78dbf30f887bbcab6ce39600794d83704
Biraj007 <birajnaskar@gmail.com>
2026-04-22 01:30:54 +0530

    feat: automate sitemap generation and fix local script paths

--- build.py
+++ build.py
@@ -1,4 +1,38 @@
-import os, glob, re
+import os, glob, re, datetime
+
+def generate_sitemap(files):
+    print("Generating sitemap.xml...")
+    base_url = "https://www.adbkanvas.com/"
+    
+    xml = '<?xml version="1.0" encoding="UTF-8"?>\n'
+    xml += '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n'
+    
+    today = datetime.datetime.now().strftime('%Y-%m-%d')
+    
+    for f in files:
+        f_norm = f.replace('\\', '/')
+        if f_norm.startswith('components/') or f_norm.startswith('vendors/'):
+            continue
+        if f_norm == '404.html':
+            continue
+            
+        # Determine priority based on page type
+        priority = "0.8"
+        if f_norm == "index.html": priority = "1.0"
+        elif "rooms" in f_norm: priority = "0.9"
+        elif "activities" in f_norm or "services" in f_norm: priority = "0.8"
+        elif "blog" in f_norm: priority = "0.7"
+        
+        # Determine URL
+        url = base_url + f_norm if f_norm != "index.html" else base_url
+        
+        xml += f'  <url>\n    <loc>{url}</loc>\n    <lastmod>{today}</lastmod>\n    <priority>{priority}</priority>\n  </url>\n'
+        
+    xml += '</urlset>'
+    
+    with open('sitemap.xml', 'w', encoding='utf-8') as sf:
+        sf.write(xml)
+    print("Successfully generated sitemap.xml!")
 
 def build_site():
     print("Building site...")
@@ -9,6 +43,9 @@ def build_site():
         footer_template = f.read()
 
     files = glob.glob('*.html') + glob.glob('*/*.html') + glob.glob('*/*/*.html')
+    
+    # Generate Sitemap automatically!
+    generate_sitemap(files)
     
     header_pattern = re.compile(r'<header[^>]*>.*?</header>', re.DOTALL | re.IGNORECASE)
     footer_pattern = re.compile(r'<footer[^>]*>.*?</footer>', re.DOTALL | re.IGNORECASE)
@@ -51,6 +88,11 @@ def build_site():
             
         new_content = header_pattern.sub(parsed_header, content)
         new_content = footer_pattern.sub(parsed_footer, new_content)
+        
+        # Quick fix to make JS script paths relative so they work locally!
+        if depth > 0:
+            new_content = new_content.replace('src="/js/', f'src="{base_url}js/')
+            new_content = new_content.replace('src="/vendors/', f'src="{base_url}vendors/')
         
         if new_content != content:
             with open(f, 'w', encoding='utf-8') as file:
